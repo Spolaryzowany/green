59500d30b67478edd9783e44f9eb6e10690ab941
PiotrKurek1999 <piotr04119@wp.pl>
2018-12-19 00:43:09 +0100

    Piata wersa v2

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+Vue.component('logo',{
+    template: `
+        <h1><span class="green">Soft</span><span class="red">Hard</span></h1>
+    `
+});
+
 var app = new Vue({
     el: '#contact',
     data: {
@@ -25,4 +31,13 @@ hideForm.onclick = function(){
     contact.classList.remove('showForm');
 }
 
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+!function(d,s,id){
+    var js,
+        fjs=d.getElementsByTagName(s)[0];
+        
+        if(!d.getElementById(id)){
+            js=d.createElement(s);
+            js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
+            fjs.parentNode.insertBefore(js,fjs);
+        }
+}(document,'script','weatherwidget-io-js');
